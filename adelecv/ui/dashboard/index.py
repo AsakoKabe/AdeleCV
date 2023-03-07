82d818ebd5450468553dd9989b2970f2e30bc19c
Denis <81520942+AsakoKabe@users.noreply.github.com>
2023-03-07 22:29:10 +0700

    update version and index

--- adelecv/ui/dashboard/index.py
+++ adelecv/ui/dashboard/index.py
@@ -53,7 +53,7 @@ def render_page_content(pathname):
 
 @click.command()
 @click.option("--envfile", '-ef', help='Path to env file', required=True)
-def main(envfile: str | None) -> None:
+def main(envfile: str = './.env') -> None:
     """
     Dashboard for AdeleCV.
     """
